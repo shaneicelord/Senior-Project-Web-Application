e7b412ae94768b361e0bc2eae3aff600eb8b3df5
n00938821 <n00938821@ospreys.unf.edu>
2017-02-09 21:44:56 -0500

    Added AdminAdjustability page titled Question.cs and the corresponding model, views and controller

---
+++ SeniorProject/SeniorProject/Models/Question.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SeniorProject.Models
+{
+    public class Question
+    {
+
+
+
+
+        public int ID { get; set; }
+
+        [StringLength(200, MinimumLength = 3)]
+        [Display(Name = "Add Question")]
+        public string Qstns { get; set; }
+
+
+    }
+
+    public class QuestionDBContext : DbContext
+    {
+        public DbSet<Question> Questions { get; set; }
+    }
+}
